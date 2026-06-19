68a87dc3246b5bbdd769fbefa2e81656cfd6a5b0
Rune Pedersen <rune@rpcloud.no>
2026-06-19 10:10:41 +0200

    Update scrolltotop.js

--- assets/js/scrolltotop.js
+++ assets/js/scrolltotop.js
@@ -1,6 +1,12 @@
-function scrollToTop(){
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("scrollTopBtn");
+
+    if (btn) {
+        btn.addEventListener("click", function () {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        });
+    }
+});
